a813b5e7cd575a1804675234a7cd9e80233660d3
GKleong <shuzhu163@sina.com>
2016-12-29 13:40:11 +0800

    edit files

--- routers/router.go
+++ routers/router.go
@@ -10,6 +10,8 @@ func init() {
 	beego.Router("/login", &controllers.LoginController{})
 	beego.Router("/category", &controllers.CategoryController{})
 	beego.Router("/topic", &controllers.TopicController{})
+	beego.Router("/replay", &controllers.ReplayController{})
 	//beego自动路由
 	beego.AutoRouter(&controllers.TopicController{})
+	beego.AutoRouter(&controllers.ReplayController{}) //留言自动路由
 }
