26a2b2729e3b1ed7889353ac746a3ef6647b575d
GKleong <shuzhu163@sina.com>
2016-12-29 14:58:02 +0800

    edit files

--- controllers/replay.go
+++ controllers/replay.go
@@ -21,9 +21,30 @@ func (this *ReplayController) Add() {
 	name := this.GetString("name")
 	email := this.GetString("email")
 	content := this.GetString("content")
+	if len(name) == 0 || len(email) == 0 || len(content) == 0 {
+		this.Redirect("/topic/views/"+tid, 302)
+		return
+	}
 	_, err := models.Replayadd(tid, name, email, content)
 	if err != nil {
 		beego.Error(err)
+		return
 	}
 	this.Redirect("/topic/views/"+tid, 302)
+}
+
+/*删除留言*/
+func (this *ReplayController) Delete() {
+	var err error
+	nrid := this.GetString("rid")
+	ntid := this.GetString("tid")
+	if len(ntid) <= 0 {
+		this.Redirect("/topic/views/"+ntid, 302)
+		return
+	}
+	_, err = models.DeleteRepaly(nrid)
+	if err != nil {
+		beego.Error(err)
+	}
+	this.Redirect("/topic/views/"+ntid, 302)
 }
